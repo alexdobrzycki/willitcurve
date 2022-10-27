fd25c8bdc8a1955dc3ab1579b836ab16b26f669c
alex dobrzycki <alexdob2003@gmail.com>
2022-10-27 00:25:22 -0400

    added calculation of curve

--- src/App.js
+++ src/App.js
@@ -4,19 +4,51 @@ import './App.css';
 
 function App() {
   const [searchText, setSearchText] = useState("");
+  const [approxAverage, setapproxAverage] = useState("");
   const [classData, setClassData] = useState({});
 
+  function isNumber(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  }
 
   function searchForClass(event) {
     // Set up API call
     var APIClassString = "https://planetterp.com/api/v1/course?name="+searchText
     // Handle API call
     axios.get(APIClassString).then(function (response) {
-      var calc = response.data.average_gpa;
-      setClassData(calc);
-      // Success
+      var dataGPA = response.data.average_gpa;
+      var gpa = parseFloat(dataGPA);
+      var average = parseFloat(approxAverage);
+      var gpaConverted;
+      if (!isNumber(gpa) || !(isNumber(average))) {
+        setClassData("Invalid input, please use the following format: \"MATH140\"");
+      } else if (gpa <= 4 && gpa > 3.7) {
+          gpaConverted = gpa * 25;
+        } else if (gpa <= 3.7 && gpa > 3.3) {
+          gpaConverted = gpa * 26;
+        } else if (gpa <= 3.3 && gpa > 3.0) {
+          gpaConverted = gpa * 27.8;
+        } else if (gpa <= 3.0 && gpa > 2.7) {
+          gpaConverted = gpa * 29.7;
+        } else if (gpa <= 2.7 && gpa > 2.3) {
+          gpaConverted = gpa * 32.54;
+        } else if (gpa <= 2.3 && gpa > 2.0) {
+          gpaConverted = gpa * 36.17;
+        } else if (gpa <= 2.0 && gpa > 1.7) {
+          gpaConverted = gpa * 40.17;
+        } else if (gpa <= 1.7 && gpa > 1.3) {
+          gpaConverted = gpa * 47.71;
+        } else if (gpa <= 1.3 && gpa >= 1.0) {
+          gpaConverted = gpa * 59.54;
+        } 
+
+        if (gpaConverted - average <= 0) {
+          setClassData("There will likely be no curve :(");
+        } else {
+          setClassData("The curve will approximately be "+ (gpaConverted - average).toFixed(2) + " points");
+        }
     }).catch(function (error) {
-      console.log(error);
+      setClassData("Invalid input, please use the following format: \"MATH140\"")
     });
   }
 
@@ -25,14 +57,17 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Will it Curve?</h1>
-        <input type="text" onChange={e => setSearchText(e.target.value)}></input>
+        <h1>Will it Curve? UMD</h1>
+        <input type="text" placeholder='Enter class, ex: MATH140' onChange={e => setSearchText(e.target.value)}></input>
+        <br></br>
+        <input type="text" placeholder='Enter approximate average'onChange={e => setapproxAverage(e.target.value)}></input>
+        <br></br>
         <button onClick={e => searchForClass(e)}>Search for a class</button>
       </div>
       {JSON.stringify(classData) !== '{}' ? 
       <><p>{classData}</p></> 
       :
-      <><p>no class data</p></> 
+      <><p>Enter Class Name:</p></> 
     }
     </div>
   );
