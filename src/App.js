cff5e406ca49eb00ec02dad767fa598143aa0d6f
alex dobrzycki <alexdob2003@gmail.com>
2022-10-26 21:02:54 -0400

    added search feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+  const [classData, setClassData] = useState({});
+
+
+  function searchForClass(event) {
+    // Set up API call
+    var APIClassString = "https://planetterp.com/api/v1/course?name="+searchText
+    // Handle API call
+    axios.get(APIClassString).then(function (response) {
+      var calc = response.data.average_gpa;
+      setClassData(calc);
+      // Success
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  console.log(classData);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Will it Curve?</h1>
+        <input type="text" onChange={e => setSearchText(e.target.value)}></input>
+        <button onClick={e => searchForClass(e)}>Search for a class</button>
+      </div>
+      {JSON.stringify(classData) !== '{}' ? 
+      <><p>{classData}</p></> 
+      :
+      <><p>no class data</p></> 
+    }
     </div>
   );
 }
